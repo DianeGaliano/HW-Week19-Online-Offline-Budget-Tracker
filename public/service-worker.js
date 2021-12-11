e362450861456a263217c5fd933e2435c916ad5a
Diane Galiano <dianegaliano09@gmail.com>
2021-12-11 17:50:31 -0600

    fixing heroku deploy errors

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,7 @@ const FILES_TO_CACHE = [
   './icon/icon-192x192.png',
   './icon/icon-192x192.png',
   'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+  'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
 ];
 
 const PRECACHE = 'precache-v1';
