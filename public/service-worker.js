50fe29c27fb5bb26470a91060a77ef223cda524a
Diane Galiano <dianegaliano09@gmail.com>
2021-12-11 20:06:40 -0600

    fixed service-worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,54 +11,44 @@ const FILES_TO_CACHE = [
   'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
 ];
 
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+const APP_PREFIX = 'BudgetTracker'
+const VERSION = 'version_01';
+const CACH_NAME = APP_PREFIX + VERSION;
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches
-      .open(PRECACHE)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-      .then(self.skipWaiting())
-  );
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache: ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
 });
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', (event) => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-      })
-      .then((cachesToDelete) => {
-        return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    catches.keys().then(function (keyList){
+      let cacheKeepList = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeepList.push(CACH_NAME);
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheKeepList.indexOf(key)=== -1) {
+          console.log('deleting cache :' + keyList[i]);
+          return caches.delete(keyList);
         }
+      }));
+    })
+  )
+});
 
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request:' + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log('responding with cache:' + e.request.url);
+        return fetch(e.request)
+      }
+    })
+  )
 });
