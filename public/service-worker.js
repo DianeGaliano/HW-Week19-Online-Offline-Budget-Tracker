f11651e78079878c1ae397fdb473b2549f169b03
Diane Galiano <dianegaliano09@gmail.com>
2021-12-20 20:48:20 -0600

    fixed heroku render issues app is working

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,8 @@ const FILES_TO_CACHE = [
   '/js/indexedDB.js',
   './icon/icon-192x192.png',
   './icon/icon-192x192.png',
- 
+  'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
+  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
 ];
 
 const APP_PREFIX = 'BudgetTracker'
